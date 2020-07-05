50a33b7d73bfb3192bcbce4807f0a74d16400f67
Mohammad-Atif <65424059+Mohammad-Atif@users.noreply.github.com>
2020-07-06 01:23:10 +0530

    intent from add event to event added

--- app/src/main/java/com/example/upcomingevents/EventsHandlingActivity.kt
+++ app/src/main/java/com/example/upcomingevents/EventsHandlingActivity.kt
@@ -1,12 +1,18 @@
 package com.example.upcomingevents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_events_handling.*
 
 class EventsHandlingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events_handling)
+        addbtn.setOnClickListener {
+            val intent=Intent(this,EventAdder::class.java)
+            startActivity(intent)
+        }
     }
 }
