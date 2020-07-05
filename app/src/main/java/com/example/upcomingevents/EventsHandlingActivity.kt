11ba35351e3f51bd97aedda5c3ea24d6cccbd7f4
Mohammad-Atif <65424059+Mohammad-Atif@users.noreply.github.com>
2020-07-06 01:01:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/upcomingevents/EventsHandlingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.upcomingevents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventsHandlingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_events_handling)
+    }
+}
