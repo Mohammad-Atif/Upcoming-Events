0ff2639a460db4583ba6c9abafe9c72a6d2349e6
Mohammad-Atif <65424059+Mohammad-Atif@users.noreply.github.com>
2020-07-06 01:45:25 +0530

    added date and event description

--- app/src/main/java/com/example/upcomingevents/EventAdder.kt
+++ app/src/main/java/com/example/upcomingevents/EventAdder.kt
@@ -2,11 +2,21 @@ package com.example.upcomingevents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_event_adder.*
 
 class EventAdder : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_adder)
+
+
+        addspecbtn.setOnClickListener {
+            if(datetxt.text.toString()=="" || describetxt.text.toString()==""){
+                Toast.makeText(this,"Enter proper info",Toast.LENGTH_LONG).show()
+
+            }
+        }
     }
 }
