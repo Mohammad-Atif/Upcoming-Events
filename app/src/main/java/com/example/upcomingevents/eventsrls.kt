9b86725c39e571b2b155a084e475f2eca4fb952c
Mohammad-Atif <65424059+Mohammad-Atif@users.noreply.github.com>
2020-07-08 18:54:20 +0530

    ui for event handling activity and icon v2

--- app/src/main/java/com/example/upcomingevents/eventsrls.kt
+++ app/src/main/java/com/example/upcomingevents/eventsrls.kt
@@ -2,7 +2,7 @@ package com.example.upcomingevents
 const val serialkey="KEY"
 var dateserials= arrayListOf<String>()
 fun updateserials() {
-    for (i in 1..20) {
+    for (i in 1..200) {
         dateserials.add("$i")
     }
 }
